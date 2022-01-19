151a9c038c519724ded95201cb9a8ee57d9d822a
Eduardo Santos <f.eduardo.santoos@gmail.com>
2022-01-19 12:19:50 -0300

    Project created

---
+++ backend/src/main/java/com/spring/esmovie/EsmovieApplication.java
@@ -0,0 +1,13 @@
+package com.spring.esmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsmovieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/spring/esmovie/EsmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.esmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
